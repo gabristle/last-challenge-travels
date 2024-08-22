8accae6d4f4c8c811684a8c29e06cc8f49f274bd
Gabriella Costa <gabx2205@gmail.com>
2024-08-22 20:10:27 -0300

    feat: details component

---
+++ frontend/src/components/Details/index.tsx
@@ -0,0 +1,70 @@
+import styles from './style.module.css'
+import localization from '../../assets/localization.png'
+import favorite from '../../assets/favorite.png'
+import pinkStar from '../../assets/pinkStar.png'
+import share from '../../assets/share.png'
+
+interface DetailsProps {
+    location: string
+    tour: string
+    costPerPerson: number
+    duration: number
+    maxPeople: number
+    minAge: number
+    type: string
+    grade: number
+    reviews: number
+}
+
+function Details({ location, tour, costPerPerson, duration, maxPeople, minAge, type, grade, reviews }: DetailsProps) {
+  return (
+    <div className={styles.detailsContainer}>
+        <div className={styles.actionsRow}>
+            <div className={styles.localization}>
+                <div className={styles.localizationRow}>
+                    <img src={localization} alt="location icon" className={styles.icon}/>
+                    <p>{location}</p>
+                </div>
+                <a href="">View on map</a>
+            </div>
+            <div className={styles.actions}>
+                <img src={share} alt="share icon" className={styles.icon}/>
+                <img src={favorite} alt="favorite icon" className={styles.icon}/>
+            </div>
+        </div>
+        <h1 className={styles.tour}>{tour}</h1>
+        <div className={styles.line}></div>
+        <div className={styles.details}>
+            <div className={styles.detailContainer}>
+                <p>From</p>
+                <p className={styles.style}>${costPerPerson}</p>
+            </div>
+            <div className={styles.detailContainer}>
+                <p>Duration</p>
+                <p>{duration} days</p>
+            </div>
+            <div className={styles.detailContainer}>
+                <p>Max People</p>
+                <p>{maxPeople}</p>
+            </div>
+            <div className={styles.detailContainer}>
+                <p>Min Age</p>
+                <p>{minAge}</p>
+            </div>
+            <div className={styles.detailContainer}>
+                <p>Tour Type</p>
+                <p>{type}</p>
+            </div>
+            <div className={styles.detailContainer}>
+                <p>Reviews</p>
+                <div className={styles.reviewRow}>
+                    <img src={pinkStar} alt="star icon" className={styles.starIcon}/>
+                    <p>{grade}.0 <span>({reviews} reviews)</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Details
