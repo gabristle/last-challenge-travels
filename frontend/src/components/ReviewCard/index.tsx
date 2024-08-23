8bac75256ffb7dbfa0cfa637a7707b5f79e8e8f1
Gabriella Costa <gabx2205@gmail.com>
2024-08-23 13:12:21 -0300

    feat: review card component

---
+++ frontend/src/components/ReviewCard/index.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import Grade from '../Grade'
+import styles from './style.module.css'
+
+interface ReviewCardProps {
+    date: string
+    author: string
+    reviews: number
+    children: ReactNode
+}
+
+function ReviewCard({ date, author, reviews, children } : ReviewCardProps) {
+  return (
+    <div className={styles.reviewCard}>
+        <div>
+            <div className={styles.image}>{author.slice(0, 2)}</div>
+        </div>
+        <div className={styles.reviewInfos}>
+            <p className={styles.date}>September {date}, 2024</p>
+            <h3 className={styles.author}>{author}</h3>
+            <div className={styles.reviewRow}>
+                <Grade grade={5}/>
+                <p className={styles.reviews}>{reviews} reviews</p>
+            </div>
+            <p className={styles.comment}>{children}</p>
+        </div>
+    </div>
+  )
+}
+
+export default ReviewCard
