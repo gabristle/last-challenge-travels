6b045b265ed2bd6aa1b2bf5e2a0d57a23129f53e
Gabriella Costa <gabx2205@gmail.com>
2024-08-23 16:34:32 -0300

    feat: pages basic structure

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
+import Login from '../pages/Login'
+import Home from '../pages/Home'
+import Tours from '../pages/Tours'
+import Tour from '../pages/Tour'
+import Error from '../pages/Error'
 
 function Router() {
   return (
     <Routes>
-        <Route path='/login'/>
-        <Route path='/'/>
-        <Route path='/tours'/>
-        <Route path='/tour'/>
-        <Route path='*'/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/tours' element={<Tours />}/>
+        <Route path='/tour' element={<Tour />}/>
+        <Route path='*' element={<Error />}/>
     </Routes>
   )
 }
