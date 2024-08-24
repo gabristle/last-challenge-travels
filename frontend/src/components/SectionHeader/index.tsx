e181c84fae1271b354a1c4bd1597fca9e29e7b19
Gabriella Costa <gabx2205@gmail.com>
2024-08-24 16:33:59 -0300

    feat: tour and destinations section

--- frontend/src/components/SectionHeader/index.tsx
+++ frontend/src/components/SectionHeader/index.tsx
@@ -1,13 +1,14 @@
 import styles from './style.module.css'
 
 interface SectionHeaderProps {
-    title: string
-    subtitle: string
+  title: string
+  subtitle: string
+  className?: string
 }
 
-function SectionHeader({ title, subtitle }: SectionHeaderProps) {
+function SectionHeader({ title, subtitle, className }: SectionHeaderProps) {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${className}`}>
         <div className={styles.titleRow}>
             <div className={styles.line}></div>
             <h1 className={styles.title}>{title}</h1>
