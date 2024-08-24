5679693cb5cf0fd24cca7a0b1e49826f1772ae67
Gabriella Costa <gabx2205@gmail.com>
2024-08-24 01:53:17 -0300

    feat: login page

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -2,15 +2,16 @@ import { ReactNode } from 'react'
 import styles from './style.module.css'
 
 interface ButtonProps {
-    children: ReactNode
-    onClick?: () => void
-    type?: 'button' | 'submit' | 'reset'
+  children: ReactNode
+  onClick?: () => void
+  type?: 'button' | 'submit' | 'reset'
+  className?: string
 }
 
-function Button({ children, onClick, type = 'button' }: ButtonProps) {
+function Button({ children, onClick, type = 'button', className }: ButtonProps) {
   return (
     <>
-        <button type={type} onClick={onClick} className={styles.button}>{children}</button>
+        <button type={type} onClick={onClick} className={`${styles.button} ${className}`}>{children}</button>
     </>
   )
 }
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,8 +1,55 @@
+import logo from '../../assets/logoTrisog.png'
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+import styles from './style.module.css'
+import googleIcon from '../../assets/google.png'
+import facebookIcon from '../../assets/facebook.png'
+
 function Login() {
+  function handleChange(){
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <main className={styles.loginBody}>
+      <section className={styles.tourSection}>
+        <div className={styles.filter}></div>
+        <div className={styles.tourInfo}>
+          <h1>Cologne Cathedral</h1>
+          <p>Cologne, Germany</p>
+        </div>
+      </section>
+      <section className={styles.loginSection}>
+        <button className={styles.backButton}>Back to home</button>
+        <div className={styles.login}>
+          <img src={logo} alt="trisog logo" className={styles.logo}/>
+          <h1 className={styles.title}>Sign in or Create account</h1>
+          <form className={styles.form}>
+            <div className={styles.inputs}>
+              <div className={styles.input}>
+                <label>Email</label>
+                <Input placeholder={'Enter your email'} onChange={handleChange}></Input>
+              </div>
+              <div className={styles.input}>
+                <label>Password</label>
+                <Input type={'password'} placeholder={'Enter your password'} onChange={handleChange}></Input>
+              </div>
+              <Button>Sign In</Button>
+              <Button className={styles.signUpButton}>Sign Up</Button>
+            </div>
+            <div className={styles.divider}>
+              <div className={styles.line}></div>
+              <p>Or sign in with</p>
+              <div className={styles.line}></div>
+            </div>
+            <div className={styles.socialButtons}>
+              <Button className={styles.googleButton}><span><img src={googleIcon} alt="google icon" />Google</span></Button>
+              <Button className={styles.facebookButton}><span><img src={facebookIcon} alt="facebook icon" />Facebook</span></Button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
   )
 }
 
