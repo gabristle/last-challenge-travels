bdc90c62040e0fbf370541dcb405660f8ed0725c
Gabriella Costa <gabx2205@gmail.com>
2024-08-24 13:19:06 -0300

    feat: home page main section

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,21 @@
+import Header from "../../components/Header"
+import LoginSignup from "../../components/LoginSignup"
+import SearchCard from "../../components/SearchCard"
+import mainImg from "../../assets/mainImage.png"
+import styles from "./style.module.css"
+
 function Home() {
   return (
     <>
+      <Header>
+        <LoginSignup/>
+      </Header>
+      <main>
+        <section className={styles.mainSection}>
+          <img src={mainImg} alt="cologne cathedral photo" className={styles.mainImg}/>
+          <SearchCard className={styles.searchCard}></SearchCard>
+        </section>
+      </main>
     </>
   )
 }
