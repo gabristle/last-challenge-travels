a440a3fe04f921ec26d42ebd8b6ebbb29e91b9ff
Gabriella Costa <gabx2205@gmail.com>
2024-08-29 14:28:49 -0300

    feat: tours main section

--- frontend/src/pages/Tours/index.tsx
+++ frontend/src/pages/Tours/index.tsx
@@ -1,13 +1,26 @@
 import Header from '../../components/Header'
 import HelloLogout from '../../components/HelloLogout'
-
+import imgHeader from '../../assets/imgHeader.png'
+import SearchCard from '../../components/SearchCard'
+import styles from './style.module.css'
+import FilterCard from '../../components/FilterCard'
 
 function Tours() {
   return (
     <>
       <Header>
-          <HelloLogout name={''}/>
+          <HelloLogout/>
       </Header>
+      <main>
+        <section className={styles.mainSection}>
+          <img src={imgHeader} alt="cathedral of cologne" />
+          <div className={styles.titleSection}>
+            <h1 className={styles.mainTitle}>Tour Package</h1>
+            <p><a href={'/'}>Home</a> / <a href={'/tours'} className={styles.tourLink}>Tour Package</a></p>
+          </div>
+          <SearchCard className={styles.searchCard}></SearchCard>
+        </section>
+      </main>
     </>
   )
 }
