dc5dc81f523cf8f802f554a2b7afe253637d1dc6
Gabriella Costa <gabx2205@gmail.com>
2024-08-29 18:13:04 -0300

    feat: map section

--- frontend/src/pages/Tour/index.tsx
+++ frontend/src/pages/Tour/index.tsx
@@ -19,6 +19,10 @@ function Tour() {
             <p className={styles.detailsText}>Istanbul, the vibrant and historic city straddling the continents of Europe and Asia, offers an enchanting blend of cultures, sights, and experiences that captivate every traveler's heart. As Turkey's cultural and economic hub, Istanbul seamlessly fuses its rich heritage with modernity, creating an unforgettable journey for visitors.</p>
             <p className={styles.detailsText}>The City is home to some of the world's most iconic landmarks, including the awe-inspiring Hagia Sophia, the majestic Blue Mosque, and the grand Topkapi Palace, each bearing witness to Istanbul's illustrious past. Wandering through the bustling streets, you'll find an array of delightful bazaars, where you can haggle for unique souvenirs, immerse yourself in the aromatic spices, and savor traditional Turkish delights.</p>
           </section>
+          <section>
+            <h2 className={styles.detailsTitle}>Map</h2>
+            
+          </section>
         </section>
         <aside className={styles.calculatorSection}>Calculator</aside>
       </main>
